99f5f1d822d3660a2fc2985c9de95d5ed8cda73e
Jesús García Valadez <jesus.garciav@me.com>
2021-03-10 13:43:42 -0600

    feat: add conversions controller

---
+++ app/Http/Controllers/ConversionsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conversions;
+use Illuminate\Http\Request;
+
+class ConversionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Conversions  $coversions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Conversions $coversions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Conversions  $coversions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Conversions $coversions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Conversions  $coversions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Conversions $coversions)
+    {
+        //
+    }
+}
