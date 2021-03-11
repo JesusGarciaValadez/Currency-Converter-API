ce058623bc031521425b6109906cf503a0d84788
Jesús García Valadez <jesus.garciav@me.com>
2021-03-10 21:55:03 -0600

    tests: fix the name of the battery of tests for the API

--- tests/Feature/CurrencyConversionApiTest.php
+++ tests/Feature/CurrencyConversionApiTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CurrencyConversionApiTests extends TestCase
+class CurrencyConversionApiTest extends TestCase
 {
     use RefreshDatabase;
 
